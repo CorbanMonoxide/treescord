0626f8c4b8eef25f1c97be86dcc24aef2636515b
CorbanMonoxide <corbanwarren@gmail.com>
2026-02-19 23:26:25 -0700

    Created a start bot batch script.

--- start_bot.bat
+++ start_bot.bat
@@ -1,9 +1,11 @@
 @echo off
 TITLE Treescord Bot
 cd /d "%~dp0"
+:loop
 echo Starting Treescord...
 call venv\Scripts\activate
 python treescord.py
 echo.
-echo Bot has stopped.
-pause
+echo Bot crashed! Restarting in 5 seconds...
+timeout /t 5
+goto loop
